2b91d7dbae86a5b082fe9f2b246d8fbdaa6d4c23
BankNatchapol <natchapol.pat@gmail.com>
2020-12-22 01:45:44 +0700

    initial project

---
+++ sql_queries.py
@@ -0,0 +1,148 @@
+# DROP TABLES
+
+songplay_table_drop = "drop table if exists songplays"
+user_table_drop = "drop table if exists users"
+song_table_drop = "drop table if exists songs"
+artist_table_drop = "drop table if exists artists"
+time_table_drop = "drop table if exists time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+    create table if not exists songplays (
+        songplay_id SERIAL primary key,
+        start_time float,
+        user_id integer,
+        song_id varchar(255),
+        artist_id varchar(255), 
+        level varchar(255),
+        session_id integer,
+        location varchar(255),
+        user_agent varchar(255),
+        foreign key (start_time)
+            references time (start_time),
+        foreign key (user_id) 
+            references users (user_id),
+        foreign key (song_id)
+            references songs (song_id),
+        foreign key (artist_id)
+            references artists (artist_id)
+    )
+""")
+
+user_table_create = ("""
+    create table if not exists users (
+        user_id integer primary key,
+        first_name varchar(255),
+        last_name varchar(255),
+        gender char(1),
+        level varchar(255)
+    )
+""")
+
+song_table_create = ("""
+    create table if not exists songs (
+        song_id varchar(255) primary key,
+        artist_id varchar(255),
+        title varchar(255),
+        year integer,
+        duration float
+        )
+""")
+
+artist_table_create = ("""
+    create table if not exists artists (
+        artist_id varchar(255) primary key,
+        name varchar(255),
+        location varchar(255),
+        latitude float,
+        longitude float
+    )
+""")
+
+time_table_create = ("""
+    create table if not exists time (
+        start_time float primary key,
+        hour integer,
+        day integer,
+        week integer,
+        month integer,
+        year integer,
+        weekday integer
+    )
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+    insert into songplays ( 
+        start_time,
+        user_id,
+        song_id,
+        artist_id, 
+        level,
+        session_id,
+        location,
+        user_agent
+    ) values ( %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+
+user_table_insert = ("""
+    insert into users (
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        level
+    ) values (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+
+song_table_insert = ("""
+    insert into songs (
+        song_id,
+        artist_id,
+        title,
+        year,
+        duration
+    ) values (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+
+artist_table_insert = ("""
+    insert into artists (
+        artist_id,
+        name,
+        location,
+        latitude,
+        longitude
+    ) values (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+
+time_table_insert = ("""
+    insert into time (
+        start_time,
+        hour,
+        day,
+        week,
+        month,
+        year,
+        weekday
+    ) values (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = ("""
+    select s.song_id, a.artist_id from songs as s 
+        join artists as a on a.artist_id = s.artist_id
+        where s.title=%s and a.name=%s and s.duration=%s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [artist_table_create, user_table_create, song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
